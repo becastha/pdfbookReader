07344426487ad558667f720d795eff0e30edc441
Bikash Shrestha <becas4th@icloud.com>
2025-12-31 16:41:06 +0900

    [modify] basic setup for health check up

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/becas/pdfbookReader/backend/internal/routes"
+)
 
 func main() {
-	fmt.Println("pdf reader")
+
+	router := routes.NewRouter()
+	log.Println("Server started in the port 8080")
+	http.ListenAndServe(":8080", router)
 }
